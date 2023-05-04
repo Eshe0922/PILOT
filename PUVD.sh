d4cb6088c307644a2868973ae2d8672761ab9a70
PUVD2023 <200111115@stu.hit.edu.cn>
2023-05-04 14:11:51 +0800

    add

--- PUVD.sh
+++ PUVD.sh
@@ -1,92 +1,96 @@
-# first finetune：
-# python run.py \
-#     --output_dir=./saved_models/train_1 \
-#     --model_type=roberta \
-#     --tokenizer_name=microsoft/codebert-base \
-#     --model_name_or_path=microsoft/codebert-base \
-#     --do_train_1\
-#     --labels_file=./data/data_result/ \
-#     --label_ratio  0.3 \
-#     --train_data_file=./data/data_preprocessed/train.txt \
-#     --eval_data_file=./data/data_preprocessed/valid.txt \
-#     --test_data_file=./data/data_preprocessed/test.txt \
-#     --epoch 2 \
-#     --block_size 512 \
-#     --train_batch_size 32 \
-#     --eval_batch_size 64 \
-#     --learning_rate 2e-5 \
-#     --max_grad_norm 1.0 \
-#     --evaluate_during_training \
-#     --seed 123456  2>&1 | tee train1.log 
+## Inter-class Distance Prototype:
 
-# execute the first step：
-# python run.py \
-#     --output_dir=./saved_models/train_1 \
-#     --model_type=roberta \
-#     --tokenizer_name=microsoft/codebert-base \
-#     --model_name_or_path=microsoft/codebert-base \
-#     --do_step1\
-#     --labels_file=./data/data_result/ \
-#     --label_ratio  0.3 \
-#     --train_data_file=./data/data_preprocessed/train.txt \
-#     --eval_data_file=./data/data_preprocessed/valid.txt \
-#     --test_data_file=./data/data_preprocessed/test.txt \
-#     --epoch 2 \
-#     --P_num 10018 \
-#     --N_num 11836 \
-#     --block_size 512 \
-#     --train_batch_size 32 \
-#     --eval_batch_size 64 \
-#     --learning_rate 2e-5 \
-#     --max_grad_norm 1.0 \
-#     --evaluate_during_training \
-#     --seed 123456  2>&1 | tee step1.log 
+# Initial fine-tuning
+python run.py \
+    --output_dir=./saved_models/train_1 \
+    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --do_train_1\
+    --labels_file=./data/data_result/ \
+    --label_ratio  0.3 \
+    --train_data_file=./data/data_preprocessed/train.txt \
+    --eval_data_file=./data/data_preprocessed/valid.txt \
+    --test_data_file=./data/data_preprocessed/test.txt \
+    --epoch 5 \
+    --block_size 512 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee train1.log 
 
-# second finetune：
-# python run.py \
-#     --output_dir=./saved_models/train_2 \
-#     --model_type=roberta \
-#     --tokenizer_name=microsoft/codebert-base \
-#     --model_name_or_path=microsoft/codebert-base \
-#     --do_train_2\
-#     --labels_file=./data/data_result/ \
-#     --label_ratio  0.3 \
-#     --train_data_file=./data/data_preprocessed/train.txt \
-#     --eval_data_file=./data/data_preprocessed/valid.txt \
-#     --test_data_file=./data/data_preprocessed/test.txt \
-#     --epoch 2 \
-#     --block_size 512 \
-#     --train_batch_size 32 \
-#     --eval_batch_size 64 \
-#     --learning_rate 2e-5 \
-#     --max_grad_norm 1.0 \
-#     --evaluate_during_training \
-#     --seed 123456  2>&1 | tee train2.log 
+# Selection of Reliable Negative (RN) samples：
+python run.py \
+    --output_dir=./saved_models/train_1 \
+    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --do_step1\
+    --labels_file=./data/data_result/ \
+    --label_ratio  0.3 \
+    --train_data_file=./data/data_preprocessed/train.txt \
+    --eval_data_file=./data/data_preprocessed/valid.txt \
+    --test_data_file=./data/data_preprocessed/test.txt \
+    --epoch 5 \
+    --P_num 10018 \
+    --N_num 11836 \
+    --block_size 512 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee step1.log 
 
-# iterative finetune：
-# python run.py \
-#     --output_dir=./saved_models/train_2 \
-#     --model_type=roberta \
-#     --tokenizer_name=microsoft/codebert-base \
-#     --model_name_or_path=microsoft/codebert-base \
-#     --do_train_iterative\
-#     --labels_file=./data/data_result/ \
-#     --label_ratio  0.3 \
-#     --train_data_file=./data/data_preprocessed/train.txt \
-#     --eval_data_file=./data/data_preprocessed/valid.txt \
-#     --test_data_file=./data/data_preprocessed/test.txt \
-#     --epoch 2 \
-#     --P_num 10018 \
-#     --N_num 11836 \
-#     --block_size 512 \
-#     --train_batch_size 32 \
-#     --eval_batch_size 64 \
-#     --learning_rate 2e-5 \
-#     --max_grad_norm 1.0 \
-#     --evaluate_during_training \
-#     --seed 123456  2>&1 | tee train_iterative.log
+## Progressive Fine-tuning: 
 
-# third finetune：
+# First step of Progressive Fine-tuning：
+python run.py \
+    --output_dir=./saved_models/train_2 \
+    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --do_train_2\
+    --labels_file=./data/data_result/ \
+    --label_ratio  0.3 \
+    --train_data_file=./data/data_preprocessed/train.txt \
+    --eval_data_file=./data/data_preprocessed/valid.txt \
+    --test_data_file=./data/data_preprocessed/test.txt \
+    --epoch 5 \
+    --block_size 512 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee train2.log 
+
+# Second step of Progressive Fine-tuning：
+python run.py \
+    --output_dir=./saved_models/train_2 \
+    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --do_train_iterative\
+    --labels_file=./data/data_result/ \
+    --label_ratio  0.3 \
+    --train_data_file=./data/data_preprocessed/train.txt \
+    --eval_data_file=./data/data_preprocessed/valid.txt \
+    --test_data_file=./data/data_preprocessed/test.txt \
+    --epoch 5 \
+    --P_num 10018 \
+    --N_num 11836 \
+    --block_size 512 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee train_iterative.log
+
+## Pesudo-label Metric Learning Module：
 python run.py \
     --output_dir=./saved_models/Model_add/train_3 \
     --model_type=roberta \
@@ -98,7 +102,7 @@ python run.py \
     --train_data_file=./data/data_preprocessed/train.txt \
     --eval_data_file=./data/data_preprocessed/valid.txt \
     --test_data_file=./data/data_preprocessed/test.txt \
-    --epoch 2 \
+    --epoch 5 \
     --block_size 512 \
     --train_batch_size 32 \
     --eval_batch_size 64 \
--- README.md
+++ README.md
@@ -42,13 +42,135 @@ FFMPeg+Qemu [3]: https://drive.google.com/file/d/1x6hoF7G-tSYxg8AFybggypLZgMGDNH
 
 To download the dataset used for evaluation in our experiments, run the following commands:
     
-    # If you want to download multiple datasets, please note the overlay of the storage location!
+Notice：If you want to download multiple datasets, please note the overlay of the storage location!
+
     cd data_raw
     gdown path_Devign(path_Reveal/path_Fan)
     cd ..
     python split data_Devign(Reveal/Fan).py
 
 ## Running the model
+
+If you want to run the model quickly, you can execute the following command:
+
+    run PUVD.sh
+
+If you want to fine-tune the parameters of each step of the model, you can execute the following command:
+
+> + For Initial fine-tuning in Inter-class Distance Prototype:
+
+    python run.py \
+        --output_dir=./saved_models/train_1 \
+        --model_type=roberta \
+        --tokenizer_name=microsoft/codebert-base \
+        --model_name_or_path=microsoft/codebert-base \
+        --do_train_1\
+        --labels_file=./data/data_result/ \
+        --label_ratio  0.3 \
+        --train_data_file=./data/data_preprocessed/train.txt \
+        --eval_data_file=./data/data_preprocessed/valid.txt \
+        --test_data_file=./data/data_preprocessed/test.txt \
+        --epoch 5 \
+        --block_size 512 \
+        --train_batch_size 32 \
+        --eval_batch_size 64 \
+        --learning_rate 2e-5 \
+        --max_grad_norm 1.0 \
+        --evaluate_during_training \
+        --seed 123456  2>&1 | tee train1.log 
+
+> + For Selection of Reliable Negative (RN) samples in Inter-class Distance Prototype:
+
+    python run.py \
+        --output_dir=./saved_models/train_1 \
+        --model_type=roberta \
+        --tokenizer_name=microsoft/codebert-base \
+        --model_name_or_path=microsoft/codebert-base \
+        --do_step1\
+        --labels_file=./data/data_result/ \
+        --label_ratio  0.3 \
+        --train_data_file=./data/data_preprocessed/train.txt \
+        --eval_data_file=./data/data_preprocessed/valid.txt \
+        --test_data_file=./data/data_preprocessed/test.txt \
+        --epoch 5 \
+        --P_num 10018 \
+        --N_num 11836 \
+        --block_size 512 \
+        --train_batch_size 32 \
+        --eval_batch_size 64 \
+        --learning_rate 2e-5 \
+        --max_grad_norm 1.0 \
+        --evaluate_during_training \
+        --seed 123456  2>&1 | tee step1.log 
+
+> + For First step in Progressive Fine-tuning:
+
+    python run.py \
+        --output_dir=./saved_models/train_2 \
+        --model_type=roberta \
+        --tokenizer_name=microsoft/codebert-base \
+        --model_name_or_path=microsoft/codebert-base \
+        --do_train_2\
+        --labels_file=./data/data_result/ \
+        --label_ratio  0.3 \
+        --train_data_file=./data/data_preprocessed/train.txt \
+        --eval_data_file=./data/data_preprocessed/valid.txt \
+        --test_data_file=./data/data_preprocessed/test.txt \
+        --epoch 5 \
+        --block_size 512 \
+        --train_batch_size 32 \
+        --eval_batch_size 64 \
+        --learning_rate 2e-5 \
+        --max_grad_norm 1.0 \
+        --evaluate_during_training \
+        --seed 123456  2>&1 | tee train2.log 
+
+> + For Second step in Progressive Fine-tuning:
+
+    python run.py \
+        --output_dir=./saved_models/train_2 \
+        --model_type=roberta \
+        --tokenizer_name=microsoft/codebert-base \
+        --model_name_or_path=microsoft/codebert-base \
+        --do_train_iterative\
+        --labels_file=./data/data_result/ \
+        --label_ratio  0.3 \
+        --train_data_file=./data/data_preprocessed/train.txt \
+        --eval_data_file=./data/data_preprocessed/valid.txt \
+        --test_data_file=./data/data_preprocessed/test.txt \
+        --epoch 5 \
+        --P_num 10018 \
+        --N_num 11836 \
+        --block_size 512 \
+        --train_batch_size 32 \
+        --eval_batch_size 64 \
+        --learning_rate 2e-5 \
+        --max_grad_norm 1.0 \
+        --evaluate_during_training \
+        --seed 123456  2>&1 | tee train_iterative.log
+
+> + For Pesudo-label Metric Learning Module：
+
+    python run.py \
+        --output_dir=./saved_models/Model_add/train_3 \
+        --model_type=roberta \
+        --tokenizer_name=microsoft/codebert-base \
+        --model_name_or_path=microsoft/codebert-base \
+        --do_train_3\
+        --labels_file=./data/data_result/ \
+        --label_ratio  0.3 \
+        --train_data_file=./data/data_preprocessed/train.txt \
+        --eval_data_file=./data/data_preprocessed/valid.txt \
+        --test_data_file=./data/data_preprocessed/test.txt \
+        --epoch 5 \
+        --block_size 512 \
+        --train_batch_size 32 \
+        --eval_batch_size 64 \
+        --learning_rate 2e-5 \
+        --max_grad_norm 1.0 \
+        --evaluate_during_training \
+        --seed 123456  2>&1 | tee train3.log
+
 ## Results
 ### Results of RQ1
 
